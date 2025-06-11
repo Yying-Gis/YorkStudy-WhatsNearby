941db775463fff8ca3178ed6c1d851ffd9aef199
Yying-Gis <yingywnb@gmail.com>
2025-06-11 20:22:01 +0800

    test

--- PY/app.py
+++ PY/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -8,3 +9,7 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "Data")
+print(f"Current directory: {os.getcwd()}")
+print(f"DATA_DIR contents: {os.listdir(DATA_DIR)}")
