6e5c17b78d6c7a8e0b3ca040be2f4155664c94da
Yying-Gis <yingywnb@gmail.com>
2025-06-11 20:29:03 +0800

    test

--- PY/app.py
+++ PY/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import osmnx as ox
+import json
+import networkx as nx
+from geopy.distance import geodesic
+import sys
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def hello_world():
     return "<p>Hello, World!</p>"
 
@@ -13,3 +20,7 @@ if __name__ == '__main__':
 DATA_DIR = os.path.join(os.path.dirname(__file__), "Data")
 print(f"Current directory: {os.getcwd()}")
 print(f"DATA_DIR contents: {os.listdir(DATA_DIR)}")
+print(f"Python version:", sys.version)
+print(f"os module test:", os.__file__)
+print(f"Current file path:", __file__)
+print(f"Data dir path:", os.path.join(os.path.dirname(__file__), "Data"))
